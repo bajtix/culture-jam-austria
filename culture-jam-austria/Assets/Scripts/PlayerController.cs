61330bcec8f48589f470257c69f7ff2d02858ee8
bajtix <bajtix.b@gmail.com>
2024-11-16 20:00:19 +0100

    player camera changes

--- culture-jam-austria/Assets/Scripts/PlayerController.cs
+++ culture-jam-austria/Assets/Scripts/PlayerController.cs
@@ -11,10 +11,13 @@ public class PlayerController : PlayerComponent {
 
     [SerializeField] private float m_speed = 4;
     [SerializeField] private float m_lookPitchLimit = 85;
+    [SerializeField] private float m_viewSens = 16, m_interactionSens = .2f;
+    [SerializeField] private float m_zeroingSpeed = 2f;
 
     private Dictionary<string, float> m_speedModifiers = new Dictionary<string, float>();
-    private KeyValuePair<string, (float, Vector3)>? m_viewModifier;
+    private KeyValuePair<string, (float, Vector3)>? m_viewModifier = null;
     private float m_yaw, m_pitch;
+    private Vector2 m_mouseOffset;
 
 
     private void Start() {
@@ -40,21 +43,31 @@ public class PlayerController : PlayerComponent {
     }
 
     private void Update() {
-        var mouseMovement = Game.Input.Player.Look.ReadValue<Vector2>();
-        m_yaw += mouseMovement.x;
-        m_pitch -= mouseMovement.y;
-        if (m_yaw > 360) m_yaw %= 360;
-        if (m_yaw < 0) m_yaw = 360 - m_yaw;
+        var mouseMovement = Game.Input.Player.Look.ReadValue<Vector2>() * Time.deltaTime;
+        if (m_viewModifier == null) {
+            m_yaw += mouseMovement.x * m_viewSens;
+            m_pitch -= mouseMovement.y * m_viewSens;
+            if (m_yaw > 360) m_yaw %= 360;
+            if (m_yaw < 0) m_yaw = 360 - m_yaw;
 
-        if (m_pitch > m_lookPitchLimit) m_pitch = m_lookPitchLimit;
-        if (m_pitch < -m_lookPitchLimit) m_pitch = -m_lookPitchLimit; // TODO: fix that
-
+            if (m_pitch > m_lookPitchLimit) m_pitch = m_lookPitchLimit;
+            if (m_pitch < -m_lookPitchLimit) m_pitch = -m_lookPitchLimit; // TODO: fix that
+        }
 
         m_camera.transform.localRotation = Quaternion.AngleAxis(m_pitch, Vector3.right);
         transform.rotation = Quaternion.AngleAxis(m_yaw, Vector3.up);
 
         if (m_viewModifier != null) {
-            var desiredLook = Quaternion.LookRotation(m_viewModifier.Value.Value.Item2 - m_camera.transform.position, Vector3.up);
+            mouseMovement *= m_interactionSens;
+            m_mouseOffset += mouseMovement;
+
+            if (mouseMovement.magnitude < 0.002f) m_mouseOffset = Vector2.Lerp(m_mouseOffset, Vector2.zero, Time.deltaTime * m_zeroingSpeed);
+            var destPoint = m_viewModifier.Value.Value.Item2;
+
+            var offsetLook = m_camera.transform.up * m_mouseOffset.y + m_camera.transform.right * m_mouseOffset.x;
+            var vec = (destPoint + offsetLook - m_camera.transform.position).normalized;
+
+            var desiredLook = Quaternion.LookRotation(vec, Vector3.up);
             m_camera.transform.rotation = Quaternion.Lerp(m_camera.transform.rotation, desiredLook, m_viewModifier.Value.Value.Item1);
         }
     }
