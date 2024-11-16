0b1e9d69eb3fa97e28b81517103afcca6e713d55
Noitiro <157133387+Noitiro@users.noreply.github.com>
2024-11-16 15:57:41 +0100

    add destroy snow

--- culture-jam-austria/Assets/Scripts/DiggingSystem.cs
+++ culture-jam-austria/Assets/Scripts/DiggingSystem.cs
@@ -1,55 +1,43 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class DiggingSystem : MonoBehaviour{
-    [SerializeField] private GameObject m_diggingArea;
     [SerializeField] private GameObject m_clickF;
-	[SerializeField] private GameObject m_Player;
-	public GameObject snow;
-	public GameObject snow2;
-	public GameObject snow3;
-	int countSnowDestroy;
-	bool inArea = false;
-	private void Update()
-	{
-		if(inArea){
-			ShowInfo();
+    GameObject[] m_snowList;
+	private int m_countSnowDestroy = 0;
+	private bool m_inArea = false;
+    void Start(){
+        m_snowList = GameObject.FindGameObjectsWithTag("Snow");
+	}
+
+	private void Update(){
+		if(m_inArea){
 			Interact();
-			Interact2();
-			Interact3();
 		}
 	}
 	void OnTriggerEnter(Collider other){
-		inArea = true;
+		m_inArea = true;
+		if(m_countSnowDestroy < 4){
+			ShowInfo(true);
+		}
 	}
-	void OnTriggerExit(Collider other)
-	{
-		inArea = false;
+	void OnTriggerExit(Collider other){
+		m_inArea = false;
+		ShowInfo(false);
 	}
-    void ShowInfo(){
-        m_clickF.SetActive(true);
+    void ShowInfo(bool status){
+        m_clickF.SetActive(status);
     }
 	void Interact(){
-		if(Input.GetButton("Fire1")) {
-			Destroy(snow);
-			countSnowDestroy++;
-		}
+			if(Input.GetButtonDown("Fire1") && m_countSnowDestroy < 4){
+				m_snowList[m_countSnowDestroy].SetActive(false);
+				m_countSnowDestroy++;
+			}
 	}
-	void Interact2(){
-		if(Input.GetButton("Fire2")) {
-			Destroy(snow2);
-			countSnowDestroy++;
-		}
-	}
-	void Interact3(){
-		if(Input.GetButton("Fire3")) {
-			Destroy(snow3);
-			countSnowDestroy++;
-		}
-	}
-	void getBelt(){
-		if(countSnowDestroy == 3){
+	void getBelt(int howMuchSnow){
+		if(m_countSnowDestroy == howMuchSnow){
 			//bierze pasek
 		}
 	}
