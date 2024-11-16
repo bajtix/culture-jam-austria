b0c77256efdb1577fbbcefd84e13318e8a50c5a3
14tp00 <118660337+14tp000@users.noreply.github.com>
2024-11-16 13:38:09 +0100

    finished staged prefab system

--- culture-jam-austria/Assets/POIScript.cs
+++ culture-jam-austria/Assets/POIScript.cs
@@ -8,6 +8,12 @@ public class POIScript : MonoBehaviour
 	private float m_timer;
 
 	private bool m_inTrigger;
+	[Header("IMPORTANT: time stamps have to be sorted in descending order")]
+	[SerializeField] private float[] m_timeStamps;
+
+	[SerializeField] private stagedPrefabScript[] m_stagedPrefabs;
+
+	private int stage = 0;
 
 	private void Start() {
 		m_timer = m_maxTimer;
@@ -17,6 +23,13 @@ public class POIScript : MonoBehaviour
 		if (m_inTrigger) {
 			m_timer -= Time.deltaTime;
 
+			if (stage < m_timeStamps.Length) {
+				if (m_timer < m_timeStamps[stage]) {
+					stage++;
+					Debug.Log("stage = " + stage);
+				}
+			}
+
 			if(m_timer <= 0) {
 				TimerFinished();
 			}
@@ -25,9 +38,9 @@ public class POIScript : MonoBehaviour
 			m_timer += Time.deltaTime * m_tickUpSpeed;
 			if (m_timer > m_maxTimer) {
 				m_timer = m_maxTimer;
-				Debug.Log("Timer maxed");
 			}
 		}
+		UpdateStagedPrefabs();
 	}
 
 	private void TimerFinished() {
@@ -46,6 +59,12 @@ public class POIScript : MonoBehaviour
 		if (other.tag == "Player") {
 			Debug.Log("Player exited " + gameObject.name + " POI");
 			m_inTrigger = false;
+		}
+	}
+
+	private void UpdateStagedPrefabs() {
+		foreach (var stagedPrefab in m_stagedPrefabs) {
+			stagedPrefab.SetTargetStage(stage);
 		}
 	}
 }
