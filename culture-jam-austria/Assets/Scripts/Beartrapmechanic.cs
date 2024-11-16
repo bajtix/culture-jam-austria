ba095fedc1c71076edd282bf1d51c16c27e75567
Halcio65 <michal.ciok.33@gmail.com>
2024-11-16 21:04:28 +0100

    added defusing trap

--- culture-jam-austria/Assets/Scripts/Beartrapmechanic.cs
+++ culture-jam-austria/Assets/Scripts/Beartrapmechanic.cs
@@ -4,15 +4,19 @@ public class Beartrapmechanic : MonoBehaviour, IInteractable {
     private float m_trapworktime = 5f;
     private bool m_isTrapActivated = false;
     private float m_timeSinceActivated = 0f;
+    private bool m_isTrapDefused = false;
+    private float m_defuseTime = 3f;
+    private float m_timeSinceDefusing = 0f;
 
-    string IInteractable.Tooltip => "Defuse trap";
+
+    string IInteractable.Tooltip => m_isTrapDefused ? "Trap defused" : "Defuse trap";
 
     private void Start() {
 
     }
 
     private void Update() {
-        if (m_isTrapActivated) {
+        if (m_isTrapActivated && !m_isTrapDefused) {
             m_timeSinceActivated += Time.deltaTime;
             if (m_timeSinceActivated >= m_trapworktime) {
                 Game.Player.Controller.RemoveSpeedModifier("Stop");
@@ -20,10 +24,14 @@ public class Beartrapmechanic : MonoBehaviour, IInteractable {
                 m_timeSinceActivated = 0f;
             }
         }
+        if (m_isTrapDefused) {
+            return;
+        }
     }
     private void OnTriggerEnter(Collider other) {
-        if (other.CompareTag("Player")) return;
+        if (other.CompareTag("Player") || m_isTrapDefused) return;
         Trap_effect();
+
     }
 
     private void Trap_effect() {
@@ -39,7 +47,7 @@ public class Beartrapmechanic : MonoBehaviour, IInteractable {
 
     }
     bool IInteractable.CanInteract(Player player) {
-        return !m_isTrapActivated;
+        return !m_isTrapDefused && !m_isTrapActivated;
     }
     bool IInteractable.CanStopInteraction(Player player) {
         return true;
@@ -50,9 +58,17 @@ public class Beartrapmechanic : MonoBehaviour, IInteractable {
     void IInteractable.InteractionStart(Player player) {
         print("Interakcja start");
         player.Controller.AddSpeedModifier("defusing", 0);
+        m_timeSinceDefusing = 0f;
+
     }
     void IInteractable.InteractionUpdate(Player player) {
-
+        m_timeSinceDefusing += Time.deltaTime;
+        if (m_timeSinceDefusing >= m_defuseTime) {
+            print("Trap defused");
+            player.Controller.RemoveSpeedModifier("defusing");
+            m_isTrapDefused = true;
+            Game.Player.Controller.RemoveSpeedModifier("Stop");
+        }
     }
     void IInteractable.InteractionFixedUpdate(Player player) {
 
@@ -60,5 +76,10 @@ public class Beartrapmechanic : MonoBehaviour, IInteractable {
     void IInteractable.InteractionEnd(Player player) {
         print("Interakcja koniec");
         player.Controller.RemoveSpeedModifier("defusing");
+        m_isTrapDefused = true;
+        Game.Player.Controller.RemoveSpeedModifier("Stop");
+        if (!m_isTrapDefused) {
+            player.Controller.RemoveSpeedModifier("defusing");
+        }
     }
 }
