67d613ab18120484632f2d60d4a60c3959354c49
Noitiro <157133387+Noitiro@users.noreply.github.com>
2024-11-16 11:49:17 +0100

    Show F button

--- culture-jam-austria/Assets/Scripts/DiggingSystem.cs
+++ culture-jam-austria/Assets/Scripts/DiggingSystem.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 
 public class DiggingSystem : MonoBehaviour{
     [SerializeField] private GameObject m_diggingArea;
+    [SerializeField] private GameObject m_clickF;
 
+    void OnCollisionEnter(Collision other)
+    {
+        ShowInfo();
+    }
 
-
+    void ShowInfo(){
+        m_clickF.SetActive(true);
+    }
 }
