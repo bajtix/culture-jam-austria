25621e51bad96c239fdbc01dc65929b48e9c80c9
bajtix <bajtix.b@gmail.com>
2024-11-16 11:49:51 +0100

    looking

--- culture-jam-austria/Assets/Scripts/PlayerController.cs
+++ culture-jam-austria/Assets/Scripts/PlayerController.cs
@@ -11,7 +11,9 @@ public class PlayerController : MonoBehaviour {
     [SerializeField][GetComponentInChildren] private Camera m_camera;
 
     private Dictionary<string, float> m_speedModifiers = new Dictionary<string, float>();
-    private Vector3 m_desiredMovement;
+    private Vector3 m_desiredLookPoint;
+
+    private float m_yaw, m_pitch;
 
     private void Start() {
         Cursor.lockState = CursorLockMode.Locked;
@@ -38,27 +40,32 @@ public class PlayerController : MonoBehaviour {
 
     private void FixedUpdate() {
         var input = Game.Input.Player.Move.ReadValue<Vector2>();
-        var mod = m_speedModifiers.Count == 0 ? 1 : m_speedModifiers.Aggregate((a, b) => new("", a.Value * b.Value)).Value;
-        var speed = m_speed * mod;
+        float speedModifier = m_speedModifiers.Count == 0 ? 1 : m_speedModifiers.Aggregate((a, b) => new("", a.Value * b.Value)).Value;
+        float speed = m_speed * speedModifier;
 
-        var transformed = transform.TransformDirection(new Vector3(
+        var movement = transform.TransformDirection(new Vector3(
             input.x, 0, input.y
         )).normalized * speed * Time.fixedDeltaTime;
 
-        m_desiredMovement.x = transformed.x;
-        m_desiredMovement.z = transformed.z;
-
         if (!m_controller.isGrounded)
-            m_desiredMovement.y += Physics.gravity.y * Time.fixedDeltaTime;
-        else m_desiredMovement.y = 0;
+            movement.y += Physics.gravity.y * Time.fixedDeltaTime;
+        else movement.y = 0;
 
-        m_controller.Move(m_desiredMovement);
+        m_controller.Move(movement);
     }
 
     private void Update() {
         var mouseMovement = Game.Input.Player.Look.ReadValue<Vector2>();
-        m_camera.transform.Rotate(Vector3.right, -mouseMovement.y);
-        transform.Rotate(Vector3.up, mouseMovement.x);
+        m_yaw -= mouseMovement.x;
+        m_pitch += mouseMovement.y;
+        if (m_yaw > 360) m_yaw = m_yaw % 360;
+        if (m_yaw < 0) m_yaw = 360 - m_yaw;
+
+        if (m_pitch > 85) m_pitch = 85;
+        if (m_pitch < -85) m_pitch = -85;
+
+        m_camera.transform.localRotation = Quaternion.Euler(m_pitch, 0, 0);
+        transform.rotation = Quaternion.Euler(0, m_yaw, 0);
     }
 
 }
