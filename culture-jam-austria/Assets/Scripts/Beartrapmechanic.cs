add1c7dedb34f2085e4026486fc928b065c216d6
Halcio65 <michal.ciok.33@gmail.com>
2024-11-16 18:59:13 +0100

    bear trap interactables

--- culture-jam-austria/Assets/Scripts/Beartrapmechanic.cs
+++ culture-jam-austria/Assets/Scripts/Beartrapmechanic.cs
@@ -1,36 +1,64 @@
 using UnityEngine;
 
-public class Beartrapmechanic : MonoBehaviour
-{
-    [SerializeField]private GameObject m_bearTrap;
+public class Beartrapmechanic : MonoBehaviour, IInteractable {
     [SerializeField] private PlayerController m_playerController;
     private float m_trapworktime = 5f;
-    private bool m_isTrapActivated= false;
+    private bool m_isTrapActivated = false;
     private float m_timeSinceActivated = 0f;
 
-    void Start(){
-        
-    }
-    void Update(){
-         if (m_isTrapActivated)
-        {
-            m_timeSinceActivated += Time.deltaTime;
-            if (m_timeSinceActivated >= m_trapworktime)
-            {
-                m_playerController.RemoveSpeedModifier("Stop");
-                m_isTrapActivated = false;
-                m_timeSinceActivated = 0f; 
-            }
-        }
-    }    
-    private void OnTriggerEnter(Collider other){
-            Trap_effect();   
-            
+    string IInteractable.Tooltip => "Defuse trap";
+
+    private void Start() {
+
     }
 
-    private void Trap_effect(){
+    private void Update() {
+        if (m_isTrapActivated) {
+            m_timeSinceActivated += Time.deltaTime;
+            if (m_timeSinceActivated >= m_trapworktime) {
+                m_playerController.RemoveSpeedModifier("Stop");
+                m_isTrapActivated = false;
+                m_timeSinceActivated = 0f;
+            }
+        }
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) return;
+        Trap_effect();
+    }
+
+    private void Trap_effect() {
         m_isTrapActivated = true;
-        m_timeSinceActivated = 0f; 
+        m_timeSinceActivated = 0f;
         m_playerController.AddSpeedModifier("Stop", 0f);
+    }
+
+    void IInteractable.HighlightBegin(Player player) {
+
+    }
+    void IInteractable.HighlightEnd(Player player) {
+
+    }
+    bool IInteractable.CanInteract(Player player) {
+        return !m_isTrapActivated;
+    }
+    bool IInteractable.CanStopInteraction(Player player) {
+        return true;
+    }
+    bool IInteractable.InteractionOver(Player player) {
+        return false;
+    }
+    void IInteractable.InteractionStart(Player player) {
+        print("Interakcja start");
+    }
+    void IInteractable.InteractionUpdate(Player player) {
+
+    }
+    void IInteractable.InteractionFixedUpdate(Player player) {
+
+    }
+    void IInteractable.InteractionEnd(Player player) {
+        print("Interakcja koniec");
+
     }
 }
