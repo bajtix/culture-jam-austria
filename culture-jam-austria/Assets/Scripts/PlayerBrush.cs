65c69c950f8cf83f12a6e99b8645d87d3760ddbd
bajtix <bajtix.b@gmail.com>
2024-11-16 20:09:52 +0100

    update player brush

--- culture-jam-austria/Assets/Scripts/PlayerBrush.cs
+++ culture-jam-austria/Assets/Scripts/PlayerBrush.cs
@@ -1,23 +1,33 @@
+using NaughtyAttributes;
 using UnityEngine;
 
-public class PlayerBrush : MonoBehaviour {
-    [SerializeField] private float m_footstepDistance;
+public class PlayerBrush : PlayerComponent {
     [SerializeField] private Texture2D m_print;
-    [SerializeField] private DrawingSurface m_surface;
+    [SerializeField] private float m_footstepDistance;
     [SerializeField] private float m_strength;
     [SerializeField] private float m_scale;
+    [SerializeField] private float m_footstepOffset = 0.05f;
+    [SerializeField][ReadOnly] private DrawingSurface m_surface;
 
     private Vector3 m_lastPosition;
     private bool m_left;
-
 
     private void FixedUpdate() {
         if ((m_lastPosition - transform.position).sqrMagnitude < m_footstepDistance) return;
         m_lastPosition = transform.position;
 
-        if (!Physics.Raycast(transform.position, Vector3.down, out var hit, 2)) return;
-        m_surface = hit.collider.GetComponent<DrawingSurface>();
-        m_surface.AddTextureMark(m_print, hit.textureCoord - new Vector2(transform.right.x * (m_left ? 0.005f : -0.005f), transform.right.z * (m_left ? 0.005f : -0.005f)), transform.rotation.eulerAngles.y * Mathf.Deg2Rad * (m_left ? 1 : -1), new Vector2(m_scale * (m_left ? 1 : -1), m_scale), m_strength);
+        void PlaceFootstep(Vector3 pos, Vector2 sc) {
+            if (!Physics.Raycast(transform.position, Vector3.down, out var hit, 2)) return;
+            m_surface = hit.collider.GetComponent<DrawingSurface>();
+
+            m_surface.AddTextureMark(m_print, hit.textureCoord, transform.rotation.eulerAngles.y, sc, m_strength);
+        }
+
+        var stepPosition = transform.position + transform.right * (m_left ? 1 : -1) * m_footstepOffset;
+        PlaceFootstep(stepPosition, new Vector2(m_left ? m_scale : -m_scale, m_scale));
+
+
+
         m_left = !m_left;
     }
 }
