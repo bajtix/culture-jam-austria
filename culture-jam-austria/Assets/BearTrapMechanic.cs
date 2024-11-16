c2d3fe7d5742653d00d5773495817ed383cd028e
Halcio65 <michal.ciok.33@gmail.com>
2024-11-16 10:51:53 +0100

    Beartrap prefab added

---
+++ culture-jam-austria/Assets/BearTrapMechanic.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class BearTrapMechanic : MonoBehaviour
+{
+    [SerializeField]private GameObject m_BearTrap;
+
+    void Start(){
+        
+    }
+    void Update(){
+        
+    }
+}
