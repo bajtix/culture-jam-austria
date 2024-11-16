266b530c20b39cfa35a157ab7d648052a36d82d5
Halcio65 <michal.ciok.33@gmail.com>
2024-11-16 13:31:18 +0100

    funcional trap

---
+++ culture-jam-austria/Assets/Scripts/Beartrapmechanic.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Beartrapmechanic : MonoBehaviour
+{
+    [SerializeField]private GameObject m_bearTrap;
+    [SerializeField] private PlayerController m_playerController;
+    private float m_trapworktime = 5f;
+    private bool m_isTrapActivated= false;
+    private float m_timeSinceActivated = 0f;
+
+    void Start(){
+        
+    }
+    void Update(){
+         if (m_isTrapActivated)
+        {
+            m_timeSinceActivated += Time.deltaTime;
+            if (m_timeSinceActivated >= m_trapworktime)
+            {
+                m_playerController.RemoveModifier("Stop");
+                m_isTrapActivated = false;
+                m_timeSinceActivated = 0f; 
+            }
+        }
+    }    
+    private void OnTriggerEnter(Collider other)
+    {
+            Trap_effect();   
+    }
+
+    private void Trap_effect()
+    {
+        m_isTrapActivated = true;
+        m_timeSinceActivated = 0f; 
+        m_playerController.AddModifier("Stop", 0f);
+    }
+}
