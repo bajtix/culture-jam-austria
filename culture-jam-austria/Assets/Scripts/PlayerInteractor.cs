d65594f69227b70b83b7770832ad3533229567da
bajtix <bajtix.b@gmail.com>
2024-11-16 15:02:19 +0100

    player look

--- culture-jam-austria/Assets/Scripts/PlayerInteractor.cs
+++ culture-jam-austria/Assets/Scripts/PlayerInteractor.cs
@@ -8,23 +8,15 @@ public class PlayerInteractor : PlayerComponent {
     [SerializeField] private LayerMask m_interactionMask = int.MaxValue;
 
     private void OnEnable() {
-        print("test");
-        Game.Input.Player.Interact.performed += (_) => Interact();
-        Game.Input.Player.Interact.started += (_) => Debug.Log("started called");
-        Game.Input.Player.Interact.performed += (_) => Debug.Log("performed called");
+        Game.Input.Player.Interact.started += (_) => Interact();
     }
 
     private void Interact() {
-        Debug.Log("i1");
         if (!Physics.Raycast(m_camera.transform.position, m_camera.transform.forward, out var hit, m_maxDistance, m_interactionMask)) return;
-        print("i2");
         var it = hit.collider.GetComponent<IInteractable>();
+        print(it);
         if (it == null) return;
 
         it.StartInteracting(Player);
     }
-
-    private void FixedUpdate() {
-    }
-
 }
