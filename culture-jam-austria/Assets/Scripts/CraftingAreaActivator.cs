a7e0b074e8a6fb6a1c91b27ac7b325622214e5ce
GabrielPL04 <gabriel.biskup08@gmail.com>
2024-11-16 17:06:46 +0100

    ProgressBarFinished

--- culture-jam-austria/Assets/Scripts/CraftingAreaActivator.cs
+++ culture-jam-austria/Assets/Scripts/CraftingAreaActivator.cs
@@ -4,6 +4,7 @@ public class CraftingAreaActivator : MonoBehaviour {
 
 	[SerializeField] private GameObject ProgressBar;
 	[SerializeField] private PlayerController m_playerController;
+	[SerializeField] private PointerController m_pointerController;
 
 	void Start()
     {
@@ -13,11 +14,13 @@ public class CraftingAreaActivator : MonoBehaviour {
 
     void Update()
     {
-        
+		if (m_pointerController.CraftingSuccess) {
+			m_playerController.RemoveSpeedModifier("Stop");
+		}
     }
 
 	private void OnTriggerEnter(Collider other) {
-		Instantiate(ProgressBar);
+		ProgressBar.SetActive(true);
 		m_playerController.AddSpeedModifier("Stop", 0f);
 	}
 }
