473758493a97ada8940799a26e200915b85066e5
Noitiro <157133387+Noitiro@users.noreply.github.com>
2024-11-16 14:14:00 +0100

    add Digging system

--- culture-jam-austria/Assets/Scripts/DiggingSystem.cs
+++ culture-jam-austria/Assets/Scripts/DiggingSystem.cs
@@ -1,15 +1,56 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DiggingSystem : MonoBehaviour{
     [SerializeField] private GameObject m_diggingArea;
     [SerializeField] private GameObject m_clickF;
-
-    void OnCollisionEnter(Collision other)
-    {
-        ShowInfo();
-    }
-
+	[SerializeField] private GameObject m_Player;
+	public GameObject snow;
+	public GameObject snow2;
+	public GameObject snow3;
+	int countSnowDestroy;
+	bool inArea = false;
+	private void Update()
+	{
+		if(inArea){
+			ShowInfo();
+			Interact();
+			Interact2();
+			Interact3();
+		}
+	}
+	void OnTriggerEnter(Collider other){
+		inArea = true;
+	}
+	void OnTriggerExit(Collider other)
+	{
+		inArea = false;
+	}
     void ShowInfo(){
         m_clickF.SetActive(true);
     }
+	void Interact(){
+		if(Input.GetButton("Fire1")) {
+			Destroy(snow);
+			countSnowDestroy++;
+		}
+	}
+	void Interact2(){
+		if(Input.GetButton("Fire2")) {
+			Destroy(snow2);
+			countSnowDestroy++;
+		}
+	}
+	void Interact3(){
+		if(Input.GetButton("Fire3")) {
+			Destroy(snow3);
+			countSnowDestroy++;
+		}
+	}
+	void getBelt(){
+		if(countSnowDestroy == 3){
+			//bierze pasek
+		}
+	}
 }
