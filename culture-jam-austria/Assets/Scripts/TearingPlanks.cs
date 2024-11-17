8ac3ef26ca1dcdf79d4e3155dce69eea20701414
Noitiro <157133387+Noitiro@users.noreply.github.com>
2024-11-17 12:38:31 +0100

    start create script

--- culture-jam-austria/Assets/Scripts/TearingPlanks.cs
+++ culture-jam-austria/Assets/Scripts/TearingPlanks.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
 
-public class TearingPlanks : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+public class TearingPlanks : MonoBehaviour, IInteractable {
+	[SerializeField] private GameObject plank;
+	void DestroyPlanks(){
+		Destroy(plank);
+	}
+	public string Tooltip => "Tearing Planks";
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public bool CanInteract(Player player) => true;
+	public bool CanStopInteraction(Player player) => false;
+	public void HighlightBegin(Player player) {
+
+	}
+	public void HighlightEnd(Player player) {
+
+	}
+	public void InteractionEnd(Player player) {
+
+	}
+	public void InteractionUpdate(Player player) {
+		var mouseMovement = Game.Input.Player.Look.ReadValue<Vector2>();
+		var leftMouseClick = Game.Input.UI.Click.IsPressed();
+		if (leftMouseClick) {
+			int i = 0;
+			do
+			if(mouseMovement.x > 15 || mouseMovement.y > 15) {
+				DestroyPlanks();
+				Debug.Log("position x: " + mouseMovement.x + " || position y: " + mouseMovement.y);
+				i++;
+			}
+			while(i < 5);
+		}
+
+	}
+	public void InteractionFixedUpdate(Player player) {
+
+	}
+	public bool InteractionOver(Player player) => false;
+	public void InteractionStart(Player player) {
+
+	}
+
 }
