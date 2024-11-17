f7c6ba2fe424b5d957b2bd45850c8b3760ec8eff
Noitiro <157133387+Noitiro@users.noreply.github.com>
2024-11-17 15:38:19 +0100

    add dottwean shake plank

--- culture-jam-austria/Assets/Scripts/TearingPlanks.cs
+++ culture-jam-austria/Assets/Scripts/TearingPlanks.cs
@@ -1,16 +1,23 @@
 using UnityEngine;
 
+using DG.Tweening;
+
 public class TearingPlanks : MonoBehaviour, IInteractable {
 	[SerializeField] private GameObject plank;
-	private int moveCount = 0;
+	[SerializeField] private GameObject m_canvasInfoTearing;
+	private float m_durationShake = 1f;
+	private float m_strengthShake = 0.5f;
+	private float m_randomnessShake = 1f;
+	private int m_vibratoShake = 1;
+	private int m_moveCount = 0;
 
-	void DestroyPlanks(){
+	void DestroyPlanks() {
 		plank.SetActive(false);
 	}
-	public string Tooltip => "Tearing Planks";
+	public string Tooltip => "Tear Planks";
 
 	public bool CanInteract(Player player) => true;
-	public bool CanStopInteraction(Player player) => true;
+	public bool CanStopInteraction(Player player) => false;
 	public void HighlightBegin(Player player) {
 
 	}
@@ -20,13 +27,20 @@ public class TearingPlanks : MonoBehaviour, IInteractable {
 	public void InteractionEnd(Player player) {
 
 	}
+	public void InteractionStart(Player player) {
+		m_canvasInfoTearing.SetActive(true);
+	}
 	public void InteractionUpdate(Player player) {
 		var mouseMovement = Game.Input.Player.Look.ReadValue<Vector2>();
 		var leftMouseClick = Game.Input.UI.Click.IsPressed();
 		if (leftMouseClick) {
-			if(mouseMovement.x > 40 || mouseMovement.y > 20) {
-				moveCount++;
-				if(moveCount >= 10){
+			m_canvasInfoTearing.SetActive(false);
+			if(mouseMovement.x > 0 || mouseMovement.y > 0) {
+				transform.DOShakePosition(m_durationShake, m_strengthShake, m_vibratoShake, m_randomnessShake);
+			}
+			if (mouseMovement.x > 30 || mouseMovement.y > 20) {
+				m_moveCount++;
+				if (m_moveCount >= 10) {
 					DestroyPlanks();
 				}
 			}
@@ -36,10 +50,8 @@ public class TearingPlanks : MonoBehaviour, IInteractable {
 	public void InteractionFixedUpdate(Player player) {
 	}
 	public bool InteractionOver(Player player) {
-		return plank.activeSelf==false;
+		return plank.activeSelf == false;
 	}
-	public void InteractionStart(Player player) {
 
-	}
 
 }
