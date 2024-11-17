f81a9355615d39c655d34f7add5c370240031f0d
GabrielPL04 <gabriel.biskup08@gmail.com>
2024-11-17 11:24:14 +0100

    Create prefab crafting skis

--- culture-jam-austria/Assets/Scripts/CraftingAreaActivator.cs
+++ culture-jam-austria/Assets/Scripts/CraftingAreaActivator.cs
@@ -6,25 +6,18 @@ using UnityEngine.SocialPlatforms.Impl;
 public class CraftingAreaActivator : MonoBehaviour, IInteractable {
 
 	[SerializeField] private GameObject m_progressBar;
-	[SerializeField] private RectTransform m_pointer;
 	[SerializeField] private TextMeshProUGUI m_scoreBar;
 
-	public RectTransform pointA;
-	public RectTransform pointB;
+	public Transform pointA;
+	public Transform pointB;
 	public RectTransform safeZone;
 	public float moveSpeed = 100f;
 	public bool CraftingSuccess = false;
 
-	public float m_direction = 1f;
+	private float m_direction = 1f;
+	private RectTransform pointerTransform;
 	private Vector3 targetPosition;
 	private float score = 0;
-	void CheckSuccess() {
-		if (RectTransformUtility.RectangleContainsScreenPoint(safeZone, m_pointer.position, null)) {
-			score++;
-		} else {
-			score = 0;
-		}
-	}
 
 	string IInteractable.Tooltip => "Craft";
 
@@ -32,24 +25,26 @@ public class CraftingAreaActivator : MonoBehaviour, IInteractable {
 		
 	}
 	void IInteractable.HighlightEnd(Player player) {
+
 	}
 	bool IInteractable.CanInteract(Player player) => true;
 	bool IInteractable.CanStopInteraction(Player player) => true;
 	bool IInteractable.InteractionOver(Player player) => false;
 	void IInteractable.InteractionStart(Player player) {
+		pointerTransform = GetComponent<RectTransform>();
 		targetPosition = pointB.position;
-		player.Controller.AddSpeedModifier("Stop", 0f);
+		Game.Player.Controller.AddSpeedModifier("Stop", 0f);
 		m_progressBar.SetActive(true);
 	}
 	void IInteractable.InteractionUpdate(Player player) {
 		m_scoreBar.text = score.ToString();
 
-		m_pointer.position = Vector3.MoveTowards(m_pointer.position, targetPosition, moveSpeed * Time.deltaTime);
+		pointerTransform.position = Vector3.MoveTowards(pointerTransform.position, targetPosition, moveSpeed * Time.deltaTime);
 
-		if (Vector3.Distance(m_pointer.position, pointA.position) < 0.1f) {
+		if (Vector3.Distance(pointerTransform.position, pointA.position) < 0.1f) {
 			targetPosition = pointB.position;
 			m_direction = 1f;
-		} else if (Vector3.Distance(m_pointer.position, pointB.position) < 0.1f) {
+		} else if (Vector3.Distance(pointerTransform.position, pointB.position) < 0.1f) {
 			targetPosition = pointA.position;
 			m_direction = -1f;
 		}
@@ -60,14 +55,20 @@ public class CraftingAreaActivator : MonoBehaviour, IInteractable {
 
 		if (score > 10) {
 			CraftingSuccess = true;
+			m_progressBar.SetActive(false);
 		}
 	}
 	void IInteractable.InteractionFixedUpdate(Player player) {
 
 	}
 	void IInteractable.InteractionEnd(Player player) {
-		m_progressBar.SetActive(false);
-		player.Controller.RemoveSpeedModifier("Stop");
+		Game.Player.Controller.RemoveSpeedModifier("Stop");
 	}
-
+	void CheckSuccess() {
+		if (RectTransformUtility.RectangleContainsScreenPoint(safeZone, pointerTransform.position, null)) {
+			score++;
+		} else {
+			score = 0;
+		}
+	}
 }
