4c23e57726886882f5722ef61111f78a7186cd8e
14tp00 <118660337+14tp000@users.noreply.github.com>
2024-11-17 14:41:30 +0100

    blizzard implemented into POIs

--- culture-jam-austria/Assets/Scripts/StagedPrefabScript.cs
+++ culture-jam-austria/Assets/Scripts/StagedPrefabScript.cs
@@ -5,7 +5,6 @@ public class StagedPrefabScript : MonoBehaviour {
 	[SerializeField] private Vector3 m_childPositionDelta;
 	private int m_stage = 0;
 	private int m_targetStage = 0;
-	private GameObject m_child;
 	private Transform m_player;
 
 
@@ -21,8 +20,6 @@ public class StagedPrefabScript : MonoBehaviour {
 	}
 
 	private void SpawnChild() {
-		//m_child = Instantiate(m_variants[m_stage], transform.position + m_childPositionDelta, transform.rotation);
-		//m_child.transform.parent = transform;
 		foreach(var variant in m_variants) {
 			variant.SetActive(false);
 		}
@@ -38,10 +35,7 @@ public class StagedPrefabScript : MonoBehaviour {
 
 	private void UpdateStage() {
 		m_stage = m_targetStage;
-		if (m_stage < m_variants.Length) {
-			Destroy(m_child, 0);
-			SpawnChild();
-		}
+		SpawnChild();
 	}
 
 	private bool CanSwap() {
