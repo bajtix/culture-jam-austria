5117c6880d6c21e5a05223dfab26df275d256c7f
bajtix <bajtix.b@gmail.com>
2024-11-23 17:39:30 +0100

    player sprinting indicatior

--- culture-jam-austria/Assets/Scripts/UIManager.cs
+++ culture-jam-austria/Assets/Scripts/UIManager.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour {
     [SerializeField] private TextMeshProUGUI m_interactionTooltip;
+    [SerializeField] private UIPlayerStatus m_uiPlayerStatus;
+
+    public UIPlayerStatus PlayerStatus => m_uiPlayerStatus;
 
     private void Start() {
         HideInteractionTooltip();
