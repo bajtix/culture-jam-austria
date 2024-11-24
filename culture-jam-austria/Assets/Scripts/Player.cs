d07e426086c8dc262cccc3fc713fb3c910a0fc77
bajtix <bajtix.b@gmail.com>
2024-11-24 16:01:03 +0100

    terrible player camera fx

--- culture-jam-austria/Assets/Scripts/Player.cs
+++ culture-jam-austria/Assets/Scripts/Player.cs
@@ -13,9 +13,11 @@ public class Player : MonoBehaviour {
     [BoxGroup("Components")][SerializeField] private PlayerController m_controller;
     [BoxGroup("Components")][SerializeField] private PlayerBrush m_brush;
     [BoxGroup("Components")][SerializeField] private PlayerInteractor m_interactor;
+    [BoxGroup("Components")][SerializeField] private PlayerCameraFx m_cameraFx;
 
     public PlayerState State => m_state;
     public PlayerController Controller => m_controller;
     public PlayerBrush Brush => m_brush;
     public PlayerInteractor Interactor => m_interactor;
+    public PlayerCameraFx CameraFx => m_cameraFx;
 }
