2d820cfa3384bf884b423bb7aa1ccdbd5302cd03
bajtix <bajtix.b@gmail.com>
2024-11-24 16:01:03 +0100

    terrible player camera fx

--- culture-jam-austria/Assets/PlayerCameraFx.cs
+++ culture-jam-austria/Assets/PlayerCameraFx.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 public class PlayerCameraFx : PlayerComponent {
+    [System.Serializable]
     private struct BobType {
         public float horizontalStrength;
         public float horizontalFrequency;
@@ -9,22 +10,59 @@ public class PlayerCameraFx : PlayerComponent {
     }
 
     [SerializeField] private Camera m_camera;
-    [SerializeField] private BobType m_walking;
+    [SerializeField] private float m_smoothing = 2;
+    [SerializeField] private BobType m_walking, m_sprinting, m_panting;
+    public Camera Camera => m_camera;
 
-
-    private float m_t;
-    private float m_l;
 
     private Vector3 m_cameraPosition;
+    private float m_bobbingTime;
+    private float m_bobbingStrength;
+    private Vector3 m_shakeVector;
+    private float m_shakeStrength;
+    private float m_shakeTimeLeft;
 
     private void Start() {
         m_cameraPosition = m_camera.transform.localPosition;
     }
 
+    private Vector3 EvaluateBobbing(BobType type, float time) {
+        return Mathf.Sin(time * type.verticalFrequency) * type.verticalStrength * Vector3.up
+                + Mathf.Cos(time * type.horizontalFrequency) * type.horizontalStrength * Vector3.right;
+    }
+
     private void Update() {
-        m_camera.transform.localPosition = m_cameraPosition;
+        float walkspeed = Mathf.Clamp01(Player.Controller.Velocity / (Player.Controller.MaxSpeed + 0.001f));
+        m_bobbingStrength = Mathf.Lerp(m_bobbingStrength, walkspeed, Time.deltaTime * m_smoothing);
+
+        Vector3 vtoa;
+
+        if (Player.Controller.IsSprinting) {
+            vtoa = EvaluateBobbing(m_sprinting, m_bobbingTime) * m_bobbingStrength;
+        } else if (Player.Controller.IsTired) {
+            vtoa = EvaluateBobbing(m_panting, Time.time) + EvaluateBobbing(m_walking, m_bobbingTime) * m_bobbingStrength;
+        } else {
+            vtoa = EvaluateBobbing(m_walking, m_bobbingTime) * m_bobbingStrength;
+        }
+        m_bobbingTime += walkspeed * Time.deltaTime;
+
+
+        m_camera.transform.localPosition = m_cameraPosition + vtoa + m_shakeVector;
+    }
+
+    private void FixedUpdate() {
+        m_shakeVector = Random.onUnitSphere * m_shakeStrength * Mathf.Clamp01(m_shakeTimeLeft);
+        if (m_shakeTimeLeft > 0) {
+            m_shakeTimeLeft -= Time.fixedDeltaTime;
+        } else {
+            m_shakeTimeLeft = 0;
+        }
+    }
+
+    public void Shake(float strength, float duration) {
+        m_shakeTimeLeft = duration;
+        m_shakeStrength = strength;
     }
 
 
-    public Camera Camera => m_camera;
 }
