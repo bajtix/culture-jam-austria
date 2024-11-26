a9d0676e4c9e062326a348bd5fb497961462e3f1
Halcio65 <michal.ciok.33@gmail.com>
2024-11-26 22:14:39 +0100

    SAW-MINIGAME 0.9

--- culture-jam-austria/Assets/Scripts/SawMinigameScript.cs
+++ culture-jam-austria/Assets/Scripts/SawMinigameScript.cs
@@ -5,39 +5,61 @@ using TMPro;
 
 public class SawMinigameScript : Interactable {
 	[SerializeField] private GameObject m_sawMinigame;
-	[SerializeField] private TMP_Text m_whichButton;
+	[SerializeField] private Image m_buttonAImage;
+	[SerializeField] private Image m_buttonDImage;
 	[SerializeField] private TMP_Text m_scoreText;
+
 	private int m_score = 0;
-	private bool m_plankiscut= false;
+	private bool m_plankIsCut = false;
 	private bool m_isA = true;
+	private float m_timer = 0f;
+	private float m_timeLimit = 2f;
+	private bool m_promptActive = false;
+	private float m_nextPromptDelay = 0f;
+
 	public override string Tooltip => "Cut Plank";
 
 	private void Start() {
 		m_isA = UnityEngine.Random.Range(0, 2) == 0;
 		UpdatePrompt();
 	}
+
 	private void Update() {
 		if (m_sawMinigame.activeSelf) {
-			IsCorrectButton();
+			m_timer += Time.deltaTime;
+			if (m_promptActive) {
+				if (m_timer > m_timeLimit) {
+					OnTimeExpired();
+				}
+				IsCorrectButton();
+			}
+			else {
+				m_nextPromptDelay -= Time.deltaTime;
+				if (m_nextPromptDelay <= 0f) {
+					ActivatePrompt();
+				}
+			}
 		}
 	}
 
-	public override bool CanInteract(Player player) => !m_plankiscut;
+	public override bool CanInteract(Player player) => !m_plankIsCut;
 
 	public override bool CanStopInteraction(Player player) => false;
 
-	public override bool InteractionOver(Player player) => m_plankiscut;
+	public override bool InteractionOver(Player player) => m_plankIsCut;
 
 	public override void InteractionStart(Player player) {
 		print("Start interaction");
 		m_sawMinigame.SetActive(true);
 		player.Controller.AddSpeedModifier("cutting", 0);
-		IsCorrectButton();
+		m_timer = 0f; 
+		UpdatePrompt();
 	}
+
 	public override void InteractionUpdate(Player player) {
-		if (m_score == 10) {
-			print("plank was cut");
-			m_plankiscut = true;
+		if (m_score == 20) { 
+			print("Plank was cut");
+			m_plankIsCut = true;
 			m_sawMinigame.SetActive(false);
 		}
 	}
@@ -45,6 +67,7 @@ public class SawMinigameScript : Interactable {
 	public override void InteractionEnd(Player player) {
 		print("Stop interaction");
 		player.Controller.RemoveSpeedModifier("cutting");
+		m_timeLimit = 2f;
 	}
 
 	private void IsCorrectButton() {
@@ -53,32 +76,60 @@ public class SawMinigameScript : Interactable {
 				m_score++;
 				print("Correct input!");
 				UpdateScoreText();
-				UpdatePrompt(); 
+				UpdatePrompt();
 			} else {
-				m_score = Mathf.Max(0, m_score - 1); 
+				m_score = Mathf.Max(0, m_score - 1);
 				print("Wrong input!");
 			}
+			m_timer = 0f;
 		} else if (Input.GetKeyDown(KeyCode.D)) {
 			if (!m_isA) {
 				m_score++;
 				print("Correct input!");
 				UpdateScoreText();
-				UpdatePrompt(); 
+				UpdatePrompt();
 			} else {
-				m_score = Mathf.Max(0, m_score - 1); 
+				m_score = Mathf.Max(0, m_score - 1);
 				print("Wrong input!");
 			}
+			m_timer = 0f;
 		}
 	}
+
 	private void UpdateScoreText() {
 		if (m_scoreText != null) {
 			m_scoreText.text = $"Score: {m_score}";
 		}
 	}
-	private void UpdatePrompt() {
-		m_isA = UnityEngine.Random.Range(0, 2) == 0;
-		m_whichButton.text = m_isA ? "Press A!" : "Press D!";
 
+	private void UpdatePrompt() {
+		m_promptActive = false;
+		m_buttonAImage.enabled = false; 
+		m_buttonDImage.enabled = false;
+		float progress = Mathf.InverseLerp(0, 20, m_score);
+		m_nextPromptDelay = Mathf.Lerp(0.3f, 0.7f, progress); 
+		m_timeLimit = Mathf.Lerp(1f, 0.3f, progress);
+	}
+
+	private void OnTimeExpired() {
+		print("Time expired!");
+		m_score = Mathf.Max(0, m_score - 1);
+		UpdateScoreText();
+		m_buttonAImage.enabled = false; 
+		m_buttonDImage.enabled = false;
+		m_promptActive = false; 
+	}
+	private void ActivatePrompt() {
+		m_promptActive = true;
+		m_isA = UnityEngine.Random.Range(0, 2) == 0;
+		if (m_isA) {
+			m_buttonAImage.enabled = true;
+			m_buttonDImage.enabled = false;
+		} else {
+			m_buttonDImage.enabled = true;
+			m_buttonAImage.enabled = false;
+		}
+		m_timer = 0f;
+		
 	}
 }
-
