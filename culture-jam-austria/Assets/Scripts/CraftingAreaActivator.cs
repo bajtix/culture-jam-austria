d0045cbdd4239aa91630a127b9480c38694634e2
Noitiro <157133387+Noitiro@users.noreply.github.com>
2024-11-26 23:41:44 +0100

    Remove Pointer Controller script and work on the hammer minigame

--- culture-jam-austria/Assets/Scripts/CraftingAreaActivator.cs
+++ culture-jam-austria/Assets/Scripts/CraftingAreaActivator.cs
@@ -2,44 +2,59 @@ using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UIElements;
 
 public class CraftingAreaActivator : Interactable {
 
 	[SerializeField] private GameObject m_progressBar;
 	[SerializeField] private TextMeshProUGUI m_scoreBar;
-
-	public Transform pointA;
-	public Transform pointB;
-	public RectTransform safeZone;
-	public float moveSpeed = 100f;
-	public bool CraftingSuccess = false;
+	[SerializeField] private GameObject m_pointer;
+	[SerializeField] private RectTransform m_pointA;
+	[SerializeField] private RectTransform m_pointB;
+	[SerializeField] private RectTransform m_safeZone;
+	[SerializeField] private float m_moveSpeed = 100f;
+	private bool m_craftingSuccess = false;
 
 	private float m_direction = 1f;
-	private Transform pointerTransform;
-	private Vector3 targetPosition;
-	private float score = 0;
+	private RectTransform m_pointerTransform;
+	private Vector3 m_targetPosition;
+	private float m_score = 0;
 
-	public override string Tooltip => "Craft";
+	public override string Tooltip => "Nail the belt to the skis";
 
-	public override bool CanInteract(Player player) => true;
-	public override bool CanStopInteraction(Player player) => true;
-	public override bool InteractionOver(Player player) => false;
-	public override void InteractionStart(Player player) {
-		pointerTransform = GetComponent<Transform>();
-		targetPosition = pointB.position;
-		Game.Player.Controller.AddSpeedModifier("Stop", 0f);
-		m_progressBar.SetActive(true);
+	private void CheckSuccess() {
+		if (RectTransformUtility.RectangleContainsScreenPoint(m_safeZone, m_pointerTransform.position, null)) {
+			m_score++;
+		} else {
+			m_score = 0;
+		}
 	}
+
+	public override bool CanInteract(Player player) => !m_craftingSuccess;
+	public override bool CanStopInteraction(Player player) => true;
+	public override bool InteractionOver(Player player) => m_craftingSuccess;
+	public override void InteractionStart(Player player) {
+		Debug.Log("->> Interaction START <<--");
+
+		m_pointerTransform = m_pointer.GetComponent<RectTransform>();
+		m_targetPosition = m_pointA.position;
+		m_progressBar.SetActive(true);
+
+		Game.Player.Controller.AddSpeedModifier("CraftingSpeed", 0f);
+		Game.Player.Controller.AddViewModifier("CraftingView", GetComponent<BoxCollider>().transform.position, 0.5f);
+	}
+
 	public override void InteractionUpdate(Player player) {
-		m_scoreBar.text = score.ToString();
+		m_scoreBar.text = "Correct hammer blows: " + m_score.ToString();
 
-		pointerTransform.position = Vector3.MoveTowards(pointerTransform.position, targetPosition, moveSpeed * Time.deltaTime);
+		m_pointerTransform.position = Vector3.MoveTowards(m_pointerTransform.position, m_targetPosition, m_moveSpeed * Time.deltaTime);
 
-		if (Vector3.Distance(pointerTransform.position, pointA.position) < 0.1f) {
-			targetPosition = pointB.position;
+		if (Vector3.Distance(m_pointerTransform.position, m_pointA.position) < 0.1f) {
+			Debug.Log(m_pointerTransform.position);
+			m_targetPosition = m_pointB.position;
 			m_direction = 1f;
-		} else if (Vector3.Distance(pointerTransform.position, pointB.position) < 0.1f) {
-			targetPosition = pointA.position;
+		} else if (Vector3.Distance(m_pointerTransform.position, m_pointB.position) < 0.1f) {
+			m_targetPosition = m_pointA.position;
 			m_direction = -1f;
 		}
 
@@ -47,19 +62,16 @@ public class CraftingAreaActivator : Interactable {
 			CheckSuccess();
 		}
 
-		if (score > 10) {
-			CraftingSuccess = true;
-			m_progressBar.SetActive(false);
+		if (m_score > 10) {
+			m_craftingSuccess = true;
 		}
 	}
+
 	public override void InteractionEnd(Player player) {
-		Game.Player.Controller.RemoveSpeedModifier("Stop");
-	}
-	private void CheckSuccess() {
-		if (RectTransformUtility.RectangleContainsScreenPoint(safeZone, pointerTransform.position, null)) {
-			score++;
-		} else {
-			score = 0;
-		}
+		Game.Player.Controller.RemoveSpeedModifier("CraftingSpeed");
+		Game.Player.Controller.RemoveViewModifier("CraftingView");
+		m_progressBar.SetActive(false);
+
+		Debug.Log("-->> Interaction END <<--");
 	}
 }
