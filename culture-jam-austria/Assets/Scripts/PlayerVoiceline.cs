9a0d3ab8e3fa006069d32273139942610a6eb56b
bajtix <bajtix.b@gmail.com>
2024-11-26 10:14:58 +0100

    playervoicelines

---
+++ culture-jam-austria/Assets/Scripts/PlayerVoiceline.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Voice Line", menuName = "Game/VoiceLine")]
+public class PlayerVoiceline : ScriptableObject {
+    [System.Serializable]
+    public enum VoiceLineType {
+        CutsceneDialogue,
+        Remark,
+        SoundEffect
+    }
+
+
+    public VoiceLineType type;
+    [Multiline] public string text;
+    public AudioClip audio;
+}
