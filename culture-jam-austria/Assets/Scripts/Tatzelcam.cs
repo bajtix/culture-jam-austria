eb5e71e92439544e1addf69c26e4678aac39a243
bajtix <bajtix.b@gmail.com>
2024-11-29 00:44:49 +0100

    camera system

--- culture-jam-austria/Assets/Scripts/Tatzelcam.cs
+++ culture-jam-austria/Assets/Scripts/Tatzelcam.cs
@@ -1,7 +1,18 @@
+using NaughtyAttributes;
 using UnityEngine;
 
 public class Tatzelcam : MonoBehaviour {
     public float fov;
     public Vector3 Position => transform.position;
     public Quaternion Rotation => transform.rotation;
+
+    [Button("Test!")]
+    private void Test() {
+        Game.Player.Cutscene.AddCamera(gameObject.name, this);
+    }
+
+    [Button("Remove!")]
+    private void Rm() {
+        Game.Player.Cutscene.PopCamera(gameObject.name);
+    }
 }
