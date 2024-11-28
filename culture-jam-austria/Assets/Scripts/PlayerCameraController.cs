f63cb846e1ccaacf01c56684aaf1e9f6df0eeef3
bajtix <bajtix.b@gmail.com>
2024-11-29 00:44:49 +0100

    camera system

--- culture-jam-austria/Assets/Scripts/PlayerCameraController.cs
+++ culture-jam-austria/Assets/Scripts/PlayerCameraController.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class PlayerCameraController : PlayerComponent {
     private Dictionary<string, (float weight, Tatzelcam cam)> m_cameras = new Dictionary<string, (float weight, Tatzelcam cam)>();
 
-    private void Update() {
+    private void LateUpdate() {
         float totalWeight = m_cameras.Sum(a => a.Value.weight);
-
 
         Vector3 position = Vector3.zero;
         Quaternion rotation = Quaternion.identity;
         float fov = 0;
 
-        foreach (var i in m_cameras) {
-            position = Vector3.Lerp(position, i.Value.cam.Position, i.Value.weight / totalWeight);
-            rotation = Quaternion.Lerp(rotation, i.Value.cam.Rotation, i.Value.weight / totalWeight);
-            fov = Mathf.Lerp(fov, i.Value.cam.fov, i.Value.weight / totalWeight);
+        if (totalWeight == 0 && m_cameras.Count > 0) {
+            position = m_cameras.First().Value.cam.Position;
+            rotation = m_cameras.First().Value.cam.Rotation;
+            fov = m_cameras.First().Value.cam.fov;
+        } else {
+            foreach (var i in m_cameras) {
+                position = Vector3.Lerp(position, i.Value.cam.Position, i.Value.weight / totalWeight);
+                rotation = Quaternion.Lerp(rotation, i.Value.cam.Rotation, i.Value.weight / totalWeight);
+                fov = Mathf.Lerp(fov, i.Value.cam.fov, i.Value.weight / totalWeight);
+            }
         }
 
         Player.Camera.transform.position = position;
--- culture-jam-austria/Assets/Scripts/PlayerCutsceneController.cs
+++ culture-jam-austria/Assets/Scripts/PlayerCutsceneController.cs
@@ -1,12 +1,66 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using UnityEngine;
 
 public class PlayerCutsceneController : PlayerComponent {
     [SerializeField] private AudioSource m_voiceSource;
 
+    private class CameraEntry {
+        public string name;
+        public Tatzelcam cam;
+        public float w;
+        public bool active;
+
+        public CameraEntry(string name, Tatzelcam cam) {
+            this.name = name;
+            this.cam = cam;
+            this.w = 0;
+            this.active = true;
+        }
+
+        public override string ToString() => $"{name} = {w}";
+    }
+
+    private List<CameraEntry> m_cameraStack = new List<CameraEntry>();
 
     private void Start() {
-        Player.CameraController.AddCam("main", Player.PlayerCamera, 1);
+        AddCamera("main", Player.PlayerCamera);
     }
+
+    public void AddCamera(string n, Tatzelcam c) {
+        if (m_cameraStack.Where(a => a.name == n).Count() != 0) {
+            throw new System.Exception("Cannot add another camera '" + n + "', it already is on the stack");
+        }
+        m_cameraStack.Add(new CameraEntry(n, c));
+    }
+
+    public void PopCamera(string n) {
+        var cams = m_cameraStack.Where(a => a.name == n);
+        if (cams.Count() < 1) {
+            Debug.LogWarning($"Cannot remove camera '{n}' as it is not on the stack.");
+            return;
+        }
+        cams.First().active = false;
+    }
+
+
+    private void Update() {
+        var lastAndActive = m_cameraStack.Where(w => w.active).Last();
+        for (int i = 0; i < m_cameraStack.Count; i++) {
+            if (!m_cameraStack[i].active && m_cameraStack[i].w < 0.001f) {
+                Player.CameraController.RemoveCam(m_cameraStack[i].name);
+                m_cameraStack.RemoveAt(i);
+            } else {
+                m_cameraStack[i].w = Mathf.Lerp(m_cameraStack[i].w, m_cameraStack[i] == lastAndActive ? 1 : 0, Time.deltaTime);
+                if (m_cameraStack[i].w < 0.001f) m_cameraStack[i].w = 0;
+                if (m_cameraStack[i].w > 1 - 0.001f) m_cameraStack[i].w = 1;
+                Player.CameraController.AddCam(m_cameraStack[i].name, m_cameraStack[i].cam, m_cameraStack[i].w);
+            }
+        }
+    }
+
+
 
     public void PlayVoiceline(PlayerVoiceline line) {
         if (line.text.Contains("{"))
