07408d6fbe8bd0f0f5413a024648bc9a4ed87a53
bajtix <bajtix.b@gmail.com>
2024-11-29 14:54:47 +0100

    bear trap stuff

---
+++ culture-jam-austria/Assets/Scripts/Games/GameBearTrap.cs
@@ -0,0 +1,84 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameBearTrap : Interactable {
+    [SerializeField] private Animator m_animator;
+    [SerializeField] private GameObject m_bloodDecal;
+    [SerializeField] private ParticleSystem m_bloodParticle;
+
+    private float m_trappingTime = 5f;
+    private bool m_isTrapActivated = false;
+    private float m_timeSinceActivated = 0f;
+    private bool m_isTrapDefused = false;
+    private float m_defuseTime = 3f;
+    private float m_timeSinceDefusing = 0f;
+
+    public override string Tooltip => m_isTrapDefused ? "Trap defused" : "Defuse trap";
+
+
+    private void Update() {
+        if (!m_isTrapActivated) return;
+
+        m_timeSinceActivated += Time.deltaTime;
+        Game.UI.SetProgress("Trapped!", m_timeSinceActivated / m_trappingTime);
+        m_animator.SetFloat("deftime", 1 - (m_timeSinceActivated / m_trappingTime));
+
+        if (m_timeSinceActivated >= m_trappingTime) {
+            StopTrapEffect();
+        }
+    }
+    private void OnTriggerEnter(Collider other) {
+        if (!other.CompareTag("Player") || m_isTrapDefused) return;
+        TrapEffect();
+    }
+
+    private void TrapEffect() {
+        Game.Player.Controller.AddSpeedModifier("Stop", 0f);
+        m_isTrapActivated = true;
+        m_timeSinceActivated = 0f;
+        m_animator.Play("snap");
+
+        if (m_bloodDecal != null) {
+            var g = Instantiate(m_bloodDecal, m_bloodDecal.transform.parent);
+            g.SetActive(true);
+            g.transform.position = m_bloodDecal.transform.position + Random.insideUnitSphere;
+            g.transform.SetParent(null);
+        }
+
+        m_bloodParticle.Play();
+    }
+
+    private void StopTrapEffect() {
+        Game.Player.Controller.RemoveSpeedModifier("Stop");
+        m_isTrapActivated = false;
+        m_timeSinceActivated = 0f;
+        Game.UI.HideProgress();
+    }
+
+
+    public override bool CanInteract(Player player) => !m_isTrapDefused && !m_isTrapActivated;
+
+    public override bool CanStopInteraction(Player player) => true;
+
+    public override bool InteractionOver(Player player) => m_timeSinceDefusing >= m_defuseTime;
+
+    public override void InteractionStart(Player player) {
+        player.Controller.AddSpeedModifier("defusing", 0);
+        m_timeSinceDefusing = 0f;
+        m_animator.Play("defusing");
+        Game.UI.SetProgress("Defusing trap", 1);
+    }
+
+    public override void InteractionUpdate(Player player) {
+        m_timeSinceDefusing += Time.deltaTime;
+        Game.UI.SetProgress("Defusing trap", m_timeSinceDefusing / m_defuseTime);
+        m_animator.SetFloat("deftime", m_timeSinceDefusing / m_defuseTime);
+    }
+
+    public override void InteractionEnd(Player player) {
+        print("Trap defused");
+        player.Controller.RemoveSpeedModifier("defusing");
+        m_isTrapDefused = true;
+        Game.UI.HideProgress();
+    }
+}
