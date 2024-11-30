76f2fa7269ecada719c9d46b48474d1cf329a52a
bajtix <bajtix.b@gmail.com>
2024-11-30 12:52:53 +0100

    belt tearing

--- culture-jam-austria/Assets/Scripts/Games/PlankTearingGame.cs
+++ culture-jam-austria/Assets/Scripts/Games/PlankTearingGame.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlankTearingGame : Interactable {
     [SerializeField] private Transform m_plank;
     [SerializeField] private GameObject m_resultingItem;
+    [SerializeField] private float m_timeItTakes;
 
     private float m_progress = 0;
 
@@ -17,17 +18,23 @@ public class PlankTearingGame : Interactable {
         m_plank.gameObject.SetActive(true);
     }
 
+    public override bool CanInteract(Player player) => m_progress < 1;
+
     public override bool InteractionOver(Player player) => m_progress >= 1;
 
     public override void InteractionStart(Player player) {
         m_progress = 0;
+        player.Controller.AddSpeedModifier("planktearing", 0f);
+        player.Controller.AddViewModifier("planktearing", m_initialPlankPosition, 0.8f);
     }
 
     public override void InteractionUpdate(Player player) {
-        m_progress += Time.deltaTime * 0.5f;
+        m_progress += Time.deltaTime / m_timeItTakes;
 
         m_plank.localPosition = Vector3.Lerp(m_initialPlankPosition, m_resultingItem.transform.localPosition, m_progress);
         m_plank.localRotation = Quaternion.Lerp(m_initialPlankRotation, m_resultingItem.transform.localRotation, m_progress);
+
+        Game.UI.SetProgress("Tearing plank", m_progress);
     }
 
     public override void InteractionEnd(Player player) {
@@ -36,5 +43,8 @@ public class PlankTearingGame : Interactable {
             m_resultingItem.transform.SetParent(null);
             m_plank.gameObject.SetActive(false);
         }
+        player.Controller.RemoveSpeedModifier("planktearing");
+        player.Controller.RemoveViewModifier("planktearing");
+        Game.UI.HideProgress();
     }
 }
