3bc05aff404edb77114b13db09a3db995a328076
bajtix <bajtix.b@gmail.com>
2024-11-30 09:18:55 +0100

    small tweaks

--- culture-jam-austria/Assets/Scripts/Games/BearTrapManager.cs
+++ culture-jam-austria/Assets/Scripts/Games/BearTrapManager.cs
@@ -1,34 +1,32 @@
+using System.Linq;
+using NaughtyAttributes;
 using UnityEngine;
 
-public class BearTrapSpawnScr : MonoBehaviour
-{
+public class BearTrapManager : StageBehaviour {
 	[SerializeField] private Transform[] m_locations;
 	[SerializeField] private Transform[] m_bearTraps;
 
-	private int m_currentPos;
 
 	private void Start() {
 		Scatter();
 	}
 
-	private void Update() {
-		//if (Game.Input.Player.Jump.WasPressedThisFrame()) {
-		//	Scatter();
-		//}
+	protected override void OnStageChanged(int s) {
+		Scatter();
 	}
 
+	[Button("Scatter")]
 	public void Scatter() {
-		Debug.Log("Scatter");
+		Debug.Log("Scattering bear traps");
 		int[] usedPos = new int[m_bearTraps.Length];
 
-		for(int i = 0;i<usedPos.Length;i++) {
+		for (int i = 0; i < usedPos.Length; i++) {
 			usedPos[i] = -1;
 		}
 
-
-		for(int i = 0;i<m_bearTraps.Length;i++){
+		for (int i = 0; i < m_bearTraps.Length; i++) {
 			int index = Random.Range(0, m_locations.Length);
-			while (InArray(usedPos, index)) {
+			while (usedPos.Contains(index)) {
 				index = Random.Range(0, m_locations.Length);
 			}
 			usedPos[i] = index;
@@ -37,12 +35,4 @@ public class BearTrapSpawnScr : MonoBehaviour
 		}
 	}
 
-	private bool InArray(int[] arr, int val) {
-		foreach(int a in arr) {
-			if(val == a) {
-				return true;
-			}
-		}
-		return false;
-	}
 }
--- culture-jam-austria/Assets/Scripts/MonsterController.cs
+++ culture-jam-austria/Assets/Scripts/MonsterController.cs
@@ -1,48 +1,35 @@
 using UnityEngine;
 using UnityEngine.Animations;
 
-public class MonsterController : MonoBehaviour
-{
-	[SerializeField] private Animator m_animator_monster;
-	[SerializeField] private Tatzelcam m_monster_cam;
-	[SerializeField] private GameObject m_game_over_screen;
+public class MonsterController : StageBehaviour {
+	[SerializeField] private Animator m_monsterAnimator;
+	[SerializeField] private Tatzelcam m_monsterCam;
 
-	private bool dead = false;
-	private bool flipflop = false;
+	private bool m_dead = false;
 
-    void Start()
-    {
-		m_animator_monster.gameObject.SetActive(false);
-		m_game_over_screen.SetActive(false);
+
+	private void Start() {
+		m_monsterAnimator.gameObject.SetActive(false);
 	}
 
-    // Update is called once per frame
-    void LateUpdate()
-    {
-		if(dead && m_animator_monster.GetCurrentAnimatorStateInfo(0).IsName("nic")) {
-			m_game_over_screen.SetActive(true);
-		}
-
-		//if (Game.Input.Player.Jump.WasPressedThisFrame()) {
-		//	if (flipflop) {
-		//		m_animator_monster.gameObject.SetActive(false);
-		//	} else {
-		//		PlayAnim();
-		//	}
-		//	flipflop = !flipflop;
-		//}
-	}
 
 	private void PlayAnim() {
-		m_animator_monster.gameObject.SetActive(true);
-		m_animator_monster.Play("Jumpscare", 0);
-		m_animator_monster.Play("Jumpscare_camera", 1);
-		Game.Player.Cutscene.AddCamera("monsterCam", m_monster_cam);
+		m_monsterAnimator.gameObject.SetActive(true);
+		m_monsterAnimator.Play("Jumpscare", 0);
+		m_monsterAnimator.Play("Jumpscare_camera", 1);
+
+		Invoke(nameof(DeathUI), m_monsterAnimator.GetCurrentAnimatorStateInfo(0).length);
+		Game.Player.Controller.enabled = false;
+		Game.Player.Cutscene.AddCamera("monsterCam", m_monsterCam);
 	}
 
-	public void Die() {
-		if (!dead) {
-			dead = true;
+	public void DeathUI() {
+		Game.UI.Dead();
+	}
+
+	protected override void OnPlayerDied() {
+		if (!m_dead) {
+			m_dead = true;
 			PlayAnim();
 		}
 	}
